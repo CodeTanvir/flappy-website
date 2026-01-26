e41d361f39ebf45f7cb4c09a4982773f8bbe6d82
CodeTanvir <info.tanvirx@gmail.com>
2026-01-27 04:16:09 +0800

    piechart fixed

--- app/(root)/(admin)/admin/dashboard/OrderStatus.jsx
+++ app/(root)/(admin)/admin/dashboard/OrderStatus.jsx
@@ -11,12 +11,12 @@ import {
 export const description = "A donut chart";
 
 const chartData = [
-  { status: "pending", visitors: 275, fill: "var(--color-pending)" },
-  { status: "processing", visitors: 200, fill: "var(--color-processign)" },
-  { status: "shipped", visitors: 187, fill: "var(--color-shipped)" },
-  { status: "delivered", visitors: 173, fill: "var(--color-delivered)" },
-  { status: "cancelled", visitors: 90, fill: "var(--color-cancelled)" },
-  { status: "unverified", visitors: 90, fill: "var(--color-unverified)" },
+  { status: "pending", count: 275, fill: "var(--color-pending)" },
+  { status: "processing", count: 200, fill: "var(--color-processing)" },
+  { status: "shipped", count: 187, fill: "var(--color-shipped)" },
+  { status: "delivered", count: 173, fill: "var(--color-delivered)" },
+  { status: "cancelled", count: 90, fill: "var(--color-cancelled)" },
+  { status: "unverified", count: 90, fill: "var(--color-unverified)" },
 ];
 
 const chartConfig = {
@@ -98,36 +98,60 @@ export function OrderStatus() {
       </ChartContainer>
       <div>
         <ul>
-            <li className="flex justify-between items-center mb-3 text-sm">
-                <span>Pending</span>
-                <span className="rounded-full 
-                px-2 text-sm bg-blue-500 text-white">0</span>
-            </li>
-               <li className="flex justify-between items-center mb-3 text-sm">
-                <span>Processing</span>
-                <span className="rounded-full 
-                px-2 text-sm bg-yellow-500 text-white">0</span>
-            </li>
-               <li className="flex justify-between items-center mb-3 text-sm">
-                <span>Shipped</span>
-                <span className="rounded-full 
-                px-2 text-sm bg-cyan-500 text-white">0</span>
-            </li>
-               <li className="flex justify-between items-center mb-3 text-sm">
-                <span>Delivered</span>
-                <span className="rounded-full 
-                px-2 text-sm bg-green-500 text-white">0</span>
-            </li>
-               <li className="flex justify-between items-center mb-3 text-sm">
-                <span>Cancelled</span>
-                <span className="rounded-full 
-                px-2 text-sm bg-red-500 text-white">0</span>
-            </li>
-               <li className="flex justify-between items-center mb-3 text-sm">
-                <span>Unverified</span>
-                <span className="rounded-full 
-                px-2 text-sm bg-orange-500 text-white">0</span>
-            </li>
+          <li className="flex justify-between items-center mb-3 text-sm">
+            <span>Pending</span>
+            <span
+              className="rounded-full 
+                px-2 text-sm bg-blue-500 text-white"
+            >
+              0
+            </span>
+          </li>
+          <li className="flex justify-between items-center mb-3 text-sm">
+            <span>Processing</span>
+            <span
+              className="rounded-full 
+                px-2 text-sm bg-yellow-500 text-white"
+            >
+              0
+            </span>
+          </li>
+          <li className="flex justify-between items-center mb-3 text-sm">
+            <span>Shipped</span>
+            <span
+              className="rounded-full 
+                px-2 text-sm bg-cyan-500 text-white"
+            >
+              0
+            </span>
+          </li>
+          <li className="flex justify-between items-center mb-3 text-sm">
+            <span>Delivered</span>
+            <span
+              className="rounded-full 
+                px-2 text-sm bg-green-500 text-white"
+            >
+              0
+            </span>
+          </li>
+          <li className="flex justify-between items-center mb-3 text-sm">
+            <span>Cancelled</span>
+            <span
+              className="rounded-full 
+                px-2 text-sm bg-red-500 text-white"
+            >
+              0
+            </span>
+          </li>
+          <li className="flex justify-between items-center mb-3 text-sm">
+            <span>Unverified</span>
+            <span
+              className="rounded-full 
+                px-2 text-sm bg-orange-500 text-white"
+            >
+              0
+            </span>
+          </li>
         </ul>
       </div>
     </div>
