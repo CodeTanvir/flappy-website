a6bc4660fdf7103ae613f4dfce7d31ffacff282e
CodeTanvir <info.tanvirx@gmail.com>
2026-01-27 01:52:10 +0800

    dasboard quick add buttons added

--- app/(root)/(admin)/admin/dashboard/page.jsx
+++ app/(root)/(admin)/admin/dashboard/page.jsx
@@ -1,9 +1,11 @@
 import CountOverview from "./CountOverview";
+import QuickAdd from "./QuickAdd";
 
 function AdminDashboard() {
   return (
     <div className="pt-5">
       <CountOverview />
+      <QuickAdd />
     </div>
   );
 }
