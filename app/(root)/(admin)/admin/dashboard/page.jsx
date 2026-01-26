7d327a2a3b7dc3e5ed8584d178e1c003376222b7
CodeTanvir <info.tanvirx@gmail.com>
2026-01-27 02:33:37 +0800

    s

--- app/(root)/(admin)/admin/dashboard/page.jsx
+++ app/(root)/(admin)/admin/dashboard/page.jsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader } from "@/components/ui/card";
+import Link from "next/link";
 import CountOverview from "./CountOverview";
 import QuickAdd from "./QuickAdd";
 
@@ -7,7 +10,17 @@ function AdminDashboard() {
       <CountOverview />
       <QuickAdd />
       <div className="mt-10 flex lg:flex-nowrap flex-warp gap-10">
-        
+        <Card className="rounded-lg lg:w-[70%] w-full p-0">
+          <CardHeader className="py-3">
+            <div className="flex justify-between items-center">
+              <span className="font-semibold">
+                <Button type="button">
+                  <Link href="">View All</Link>
+                </Button>
+              </span>
+            </div>
+          </CardHeader>
+        </Card>
       </div>
     </div>
   );
