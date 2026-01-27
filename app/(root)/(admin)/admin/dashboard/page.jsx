2aa4e2e16fc2d622a1cd2f28e11af2ba2abb0487
CodeTanvir <info.tanvirx@gmail.com>
2026-01-28 00:11:25 +0800

    Orderlist Ui

--- app/(root)/(admin)/admin/dashboard/page.jsx
+++ app/(root)/(admin)/admin/dashboard/page.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Link from "next/link";
 import CountOverview from "./CountOverview";
+import LatestOrder from "./LatestOrder";
 import { OrderOverview } from "./OrderOverview";
 import { OrderStatus } from "./OrderStatus";
 import QuickAdd from "./QuickAdd";
@@ -37,6 +38,32 @@ function AdminDashboard() {
           <CardContent>
             <OrderStatus />
           </CardContent>
+        </Card>
+      </div>
+      <div className="mt-10 flex lg:flex-nowrap flex-warp gap-10">
+        <Card className="rounded-lg lg:w-[70%] w-full p-0 block">
+          <CardHeader className="py-3 border [.border-b]:pb-3">
+            <div className="flex justify-between items-center">
+              <span className="font-semibold">Latest Order</span>
+              <Button type="button">
+                <Link href="">View All</Link>
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent className="pt-3">
+            <LatestOrder />
+          </CardContent>
+        </Card>
+        <Card className="rounded-lg lg:w-[30%] w-full p-0">
+          <CardHeader className="py-3 border [.border-b]:pb-3">
+            <div className="flex justify-between items-center">
+              <span className="font-semibold">Latest Review</span>
+              <Button type="button">
+                <Link href="">View All</Link>
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent></CardContent>
         </Card>
       </div>
     </div>
